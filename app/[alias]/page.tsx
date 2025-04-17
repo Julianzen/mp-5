ad9727c124989153d7290319eadcbc9bf60055a5
Julian Blackthorn <54967864+Julianzen@users.noreply.github.com>
2025-04-17 00:42:24 -0400

    Update page.tsx

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -5,9 +5,7 @@ import { redirect } from "next/navigation";
 
 export default async function RedirectPage({ params }: { params: { alias: string } }) {
     const db = await getDb();
-    console.log(params.alias)
     const found = await db.collection("urls").findOne({ alias: params.alias });
-    console.log(found)
     if (!found) {
         return <div className="p-8 text-red-500">Alias not found</div>;
     }
