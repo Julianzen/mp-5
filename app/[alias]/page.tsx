a2acc0ef365f73276b35439b677e5faa1d34cd86
Julian Blackthorn <54967864+Julianzen@users.noreply.github.com>
2025-04-17 01:34:22 -0400

    Update page.tsx

--- app/[alias]/page.tsx
+++ app/[alias]/page.tsx
@@ -3,12 +3,7 @@ export const dynamic = 'force-dynamic';
 import { getDb } from "@/lib/mongodb";
 import { redirect } from "next/navigation";
 
-interface Props {
-  params: {
-    alias: string;
-  };
-}
-export default async function RedirectPage({ params }: { params: { alias: string } }) {
+export default async function RedirectPage({ params, }: { params: { alias: string } }) {
     const db = await getDb();
     const found = await db.collection("urls").findOne({ alias: params.alias });
     if (!found) {
